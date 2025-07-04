ffdd16f0497c5137a4e11c1c6c56f4e15e9f179d
Nazar adil <nazaradil.dev@gmail.com>
2025-07-04 14:02:15 +0000

    اريد التاثير للزوم ان يستمر حتي تغيير الصورة وان يكون بطئ

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,7 +93,7 @@ export default {
             transform: 'scale(1)',
           },
           '100%': {
-            transform: 'scale(1.1)',
+            transform: 'scale(1.05)',
           },
         },
       },
